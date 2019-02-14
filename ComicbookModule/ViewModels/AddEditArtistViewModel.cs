ecb5d5bd789797738600db044f025a245354ca1a
SRewo <maoam97@gmail.com>
2019-02-14 10:23:43 +0100

    Add artist list/edit views and navigation

---
+++ ComicbookModule/ViewModels/AddEditArtistViewModel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ComicBookShop.Data;
+using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Regions;
+
+namespace ComicbookModule.ViewModels
+{
+    public class AddEditArtistViewModel : BindableBase, INavigationAware
+    {
+        private IRegionManager _regionManager;
+        private IRepository<Artist> _artistRepository;
+        public DelegateCommand GoBackCommand { get; private set; }
+        public DelegateCommand FirstNameChangedCommand { get; private set; }
+        public DelegateCommand LastNameChangedCommand { get; private set; }
+
+        private Artist _artist;
+
+        public Artist Artist
+        {
+            get => _artist;
+            set => SetProperty(ref _artist, value);
+        }
+
+        public AddEditArtistViewModel(IRegionManager manager)
+        {
+
+            _regionManager = manager;
+            GoBackCommand = new DelegateCommand(GoBack);
+
+        }
+
+        private void GoBack()
+        {
+            
+            _regionManager.RequestNavigate("content","ArtistList");
+
+        }
+
+        public bool IsNavigationTarget(NavigationContext navigationContext)
+        {
+            return true;
+        }
+
+        public void OnNavigatedFrom(NavigationContext navigationContext)
+        {
+            
+        }
+
+        public void OnNavigatedTo(NavigationContext navigationContext)
+        {
+            Artist = new Artist();
+        }
+    }
+}
---
+++ ComicbookModule/ViewModels/ArtistListViewModel.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using ComicBookShop.Data;
+using ComicBookShop.Data.Repositories;
+using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Regions;
+
+namespace ComicbookModule.ViewModels
+{
+    public class ArtistListViewModel : BindableBase, INavigationAware
+    {
+
+        private IRepository<Artist> _artistRepository;
+        private List<Artist> _allArtists;
+        private IRegionManager _regionManager;
+        public DelegateCommand AddArtistCommand { get; private set; }
+        public DelegateCommand EditArtistCommand { get; private set; }
+
+        private List<Artist> _viewList;
+
+        public List<Artist> ViewList
+        {
+            get => _viewList;
+            set => SetProperty(ref _viewList, value);
+        }
+
+        private string _searchWord;
+
+        public string SearchWord
+        {
+            get => _searchWord;
+            set => SetProperty(ref _searchWord, value);
+        }
+
+        private Artist _artist;
+
+        public Artist Artist
+        {
+            get => _artist;
+            set => SetProperty(ref _artist, value);
+        }
+
+        public ArtistListViewModel(IRegionManager manager)
+        {
+
+            _regionManager = manager;
+            AddArtistCommand = new DelegateCommand(OpenAdd);
+            EditArtistCommand = new DelegateCommand(OpenEdit);
+
+            using (var context = new ShopDbEntities())
+            {
+                
+                _artistRepository = new SqlRepository<Artist>(context);
+                _allArtists = _artistRepository.GetAll().ToList();
+                ViewList = _allArtists;
+
+            }
+
+        }
+
+        private void OpenAdd()
+        {
+            
+            _regionManager.RequestNavigate("content","AddEditArtist");
+
+        }
+
+        private void OpenEdit()
+        {
+            if (Artist == null)
+                MessageBox.Show("You have to choose artist.");
+            else
+            {
+                NavigationParameters parameters = new NavigationParameters();
+                parameters.Add("Artist",Artist);
+                _regionManager.RequestNavigate("content","AddEditArtist",parameters);
+            }
+        }
+
+        public bool IsNavigationTarget(NavigationContext navigationContext)
+        {
+            return true;
+        }
+
+        public void OnNavigatedFrom(NavigationContext navigationContext)
+        {
+
+        }
+
+        public void OnNavigatedTo(NavigationContext navigationContext)
+        {
+
+        }
+    }
+}
